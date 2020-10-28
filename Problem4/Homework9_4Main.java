0e2cad3bd3335c578c8ff0276891ef78d3e1ab4b
Charlesccharris <harriscc@student.swosu.edu>
2020-10-27 22:54:04 -0500

    Haven't started the "Random Class" problem

---
+++ Problem4/Homework9_4Main.java
@@ -0,0 +1,13 @@
+public class Homework9_4Main{
+	public static void main(String[] args){
+		/*Modified: *9.4 (Use the Random class)
+		Write a program that creates a Random object
+		with seed 1000 and displays the first 5 random integers
+		between 0 and 100 using the nextInt(100) method.
+		Repeat the list three times.*/
+
+		System.out.println("Hello");
+		SayHello hi = new SayHello();
+		hi.sayHello();
+	}
+}
