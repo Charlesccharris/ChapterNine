a2279bbbd4918ebc22e5f2537a564f4419baa77a
Charlesccharris <harriscc@student.swosu.edu>
2020-10-30 09:16:10 -0500

    Changed some of the formatting

--- Problem4/Homework9_4Main.java
+++ Problem4/Homework9_4Main.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Homework9_4Main{
 	public static void main(String[] args){
 		/*Modified: *9.4 (Use the Random class)
@@ -6,8 +8,16 @@ public class Homework9_4Main{
 		between 0 and 100 using the nextInt(100) method.
 		Repeat the list three times.*/
 
-		System.out.println("Hello");
-		SayHello hi = new SayHello();
-		hi.sayHello();
+		int[] randomList = new int[5];
+		Random numbers = new Random(1000);
+		for(int i = 0; i < 5; i++){
+			randomList[i] = numbers.nextInt(100);
+		}
+		for(int j = 0; j < 3; j++){
+			for(int k = 0; k < 5; k++){
+				System.out.print(randomList[k] + " ");
+			}
+			System.out.println("");
+		}
 	}
 }
